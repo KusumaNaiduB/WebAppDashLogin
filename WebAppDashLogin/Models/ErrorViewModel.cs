e74da9b4b4a0d5a1091f3ebdbd70bc599637db26
KusumaNaiduB <kusumanaidu8@gmail.com>
2024-02-25 20:32:25 +0530

    Add project files.

---
+++ WebAppDashLogin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppDashLogin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
